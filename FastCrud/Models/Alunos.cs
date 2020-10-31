66a276414c264899856591e952ccbd5deba6d497
Eduardo Aguiar <eduardo.aguiar@k2partnering.com>
2020-10-31 15:29:20 -0300

    Add project files.

---
+++ FastCrud/Models/Alunos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FastCrud.Models
+{
+    public partial class Alunos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Matricula { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ FastCrud/Models/EscolaContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FastCrud.Models
+{
+    public partial class EscolaContext : DbContext
+    {
+        public EscolaContext()
+        {
+        }
+
+        public EscolaContext(DbContextOptions<EscolaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Alunos> Alunos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Alunos>(entity =>
+            {
+                entity.Property(e => e.DataCadastro).HasColumnType("datetime");
+
+                entity.Property(e => e.DataNascimento).HasColumnType("datetime");
+
+                entity.Property(e => e.Email).HasMaxLength(200);
+
+                entity.Property(e => e.Matricula).HasMaxLength(15);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(150);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
